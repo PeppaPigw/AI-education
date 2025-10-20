c3e824d516494f2eff65f559951edd13bfc5aaab
Quixotica11y <peppapigwqy@gmail.com>
2025-10-20 22:30:54 +0800

    LORA

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import tools.language_handler as lh
-
-
-@pytest.fixture
-def temp_config_path(tmp_path, monkeypatch):
-    config = tmp_path / "user_config.json"
-    monkeypatch.setattr(lh, "CONFIG_PATH", str(config))
-    return config
